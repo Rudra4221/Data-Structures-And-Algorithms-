6497d264ee25ab9ef6b099407a3342cef197ccc0
Rudra4221 <rudra260704@gmail.com>
2025-11-12 23:04:25 +0000

    SpiralMatrix

---
+++ Data Structures And Algorithms/Day -2/2D-Arrays/SprialMatrix.java
@@ -0,0 +1,52 @@
+
+public class SprialMatrix {
+
+    public static void printSpiral(int arr[][]) {
+
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = arr.length - 1;
+        int endCol = arr[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            for (int i = startCol; i <= endCol; i++) {
+                System.out.print(arr[startRow][i] + " ");
+            }
+
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(arr[i][endCol] + " ");
+            }
+
+            for (int i = endCol - 1; i >= startCol; i--) {
+                if (startRow == endRow) {
+                    break;
+                }
+                System.out.print(arr[endRow][i] + " ");
+            }
+
+            for (int i = endRow - 1; i >= startRow; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                System.out.print(arr[i][startCol] + " ");
+            }
+
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[][] = { { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 24, 25 }
+        };
+
+        printSpiral(arr);
+    }
+}
