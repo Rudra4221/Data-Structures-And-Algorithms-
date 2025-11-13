ee558b76477e33b6d8379ee886996e4b8d615b79
Rudra4221 <rudra260704@gmail.com>
2025-11-13 23:47:43 +0000

    is Palindrome

--- Data Structures And Algorithms/Day-3/SpiralPrint.java
+++ Data Structures And Algorithms/Day-3/SpiralPrint.java
@@ -42,9 +42,9 @@ public class SpiralPrint {
     public static void main(String[] args) {
 
         int arr[][] = { { 1, 2, 3 }, 
-        { 4, 5, 6 },
-         { 7, 8, 9 }, 
-         {10,11,12} };
+                        { 4, 5, 6 },
+                        { 7, 8, 9 }, 
+                        {10,11,12} };
 
         printSpiral(arr);
 
---
+++ Data Structures And Algorithms/Day-3/Strings/CharAT.java
@@ -0,0 +1,12 @@
+
+public class CharAT {
+
+    public static void main(String[] args) {
+
+        String str = "Rudra Patel";
+
+        for (int i = 0; i <= str.length(); i++) {
+            System.out.println(i + ")" + str.charAt(i));
+        }
+    }
+}
